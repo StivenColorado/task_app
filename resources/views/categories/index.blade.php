bfa51f0136fdaa96c32279e30216ba6baab437eb
stiven <stivenchoo@gmail.com>
2024-05-02 11:55:08 -0500

    improving design

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -29,60 +29,71 @@
             </form>
             <div>
                 @foreach ($categories as $category)
-                    <a class="text-decoration-none text-white"
-                        href="{{ route('categories.show', ['category' => $category->id]) }}">
-                        <div class="row py-1 w-auto m-2 rounded item-todo d-flex justify-content-center align-items-center">
-                            <div class="col-md-9 d-flex align-items-center">
-                                <div class="p-2 m-2" style="background-color: {{ $category->color }}"></div>
+                    <div class="row py-1 w-auto m-2 rounded item-todo d-flex justify-content-center align-items-center">
 
-                                {{ $category->name }}
-                            </div>
+                        <div class="col-md-9 d-flex align-items-center">
+                            <div class="p-2 m-2" style="background-color: {{ $category->color }}"></div>
 
-                            <div class="col-md-3 d-flex justify-content-end align-items-center">
-                                <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                                    data-bs-target="#{{ $category->id }}">
+                            {{ $category->name }}
+                        </div>
 
+                        <div class="col-md-3 d-flex justify-content-end align-items-center gap-1">
+                            <a class="text-decoration-none text-white"
+                                href="{{ route('categories.show', ['category' => $category->id]) }}">
+                                <button class="btn btn-warning btn-sm">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22"
                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                         stroke-linecap="round" stroke-linejoin="round"
-                                        class="icon icon-tabler icons-tabler-outline icon-tabler-trash">
+                                        class="icon icon-tabler icons-tabler-outline icon-tabler-edit">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                        <path d="M4 7l16 0" />
-                                        <path d="M10 11l0 6" />
-                                        <path d="M14 11l0 6" />
-                                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                        <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
+                                        <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
+                                        <path d="M16 5l3 3" />
                                     </svg>
                                 </button>
-                            </div>
-                        </div>
+                            </a>
+                            <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
+                                data-bs-target="#{{ $category->id }}">
 
-                        <div class="modal fade" id="{{ $category->id }}" tabindex="-1" role="dialog"
-                            aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        deseas eliminar la categoria "{{ $category->name }}" ?
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        <form action="{{ route('categories.destroy', ['category' => $category->id]) }}"
-                                            method="POST">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" class="btn btn-primary">Eliminar</button>
-                                        </form>
-                                    </div>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-trash">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M4 7l16 0" />
+                                    <path d="M10 11l0 6" />
+                                    <path d="M14 11l0 6" />
+                                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="modal fade" id="{{ $category->id }}" tabindex="-1" role="dialog"
+                        aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    deseas eliminar la categoria "{{ $category->name }}" ?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <form action="{{ route('categories.destroy', ['category' => $category->id]) }}"
+                                        method="POST">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary">Eliminar</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
-                    </a>
+                    </div>
                 @endforeach
             </div>
         </div>
